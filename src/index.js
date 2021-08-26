8162cedd5521c6fb00ef96bb968734f940f386cb
DistrugatorDeManele <radustoican1@gmail.com>
2021-08-26 18:06:39 +0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './Homepage';
 import Game from './Game';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { Auth0ProviderWithHistory } from "@auth0/auth0-react";
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const io = require('socket.io-client');
 const socket = io('http://localhost:3000/');
@@ -15,11 +14,9 @@ ReactDOM.render(
           <Game socket={socket} />
         </Route>
         <Route path="/">
-          <Auth0Provider>
-            <Auth0ProviderWithHistory>
-              <Homepage socket={socket} />
-            </Auth0ProviderWithHistory>
-          </Auth0Provider>
+          <Auth0ProviderWithHistory>
+            <Homepage socket={socket} />
+          </Auth0ProviderWithHistory>
         </Route>
       </Switch>
     </Router>
